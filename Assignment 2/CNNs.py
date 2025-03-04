6024b538231b3be69ab4b27a37c8068094e43cf7
Jonas Leeb <ge59duw@mytum.de>
2025-03-04 16:46:44 +0100

    Residuals

--- Assignment 2/CNNs.py
+++ Assignment 2/CNNs.py
@@ -6,47 +6,92 @@ import torch.nn.functional as F
 
 
 class SimpleConvNet(pl.LightningModule):
-    def __init__(self, in_channels=3, num_classes=1, dropout_rate=0, conv_channels=[16, 32], linear_features=[60]):
+    def __init__(self, in_channels=3, num_classes=1, dropout_rate=0, conv_channels=[16, 32], linear_features=[60], use_residuals=False):
         super().__init__()
         #######################
         # PUT YOUR CODE HERE  #
         #######################
-        self.layers = nn.Sequential(
-            # conv block 1
-            nn.Conv2d(in_channels=in_channels, out_channels=conv_channels[0], kernel_size=3, padding=1),
-            nn.BatchNorm2d(conv_channels[0]),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
 
-            # conv block 2 to n
-            *[
+        if use_residuals:
+            self.block1= nn.Sequential(
+                # conv block 1
+                nn.Conv2d(in_channels=in_channels, out_channels=conv_channels[0], kernel_size=3, padding=1),
+                nn.BatchNorm2d(conv_channels[0]),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2))
+
+            # Res block
+            self.res_blocks = nn.ModuleList([
                 nn.Sequential(
-                    nn.Conv2d(in_channels=conv_channels[i], out_channels=conv_channels[i+1], kernel_size=3, padding=1),
-                    nn.BatchNorm2d(conv_channels[i+1]),
+                    nn.Conv2d(conv_channels[i], conv_channels[i + 1], kernel_size=3, padding=1),
+                    nn.BatchNorm2d(conv_channels[i + 1]),
                     nn.ReLU(),
                     nn.MaxPool2d(2, 2)
                 ) for i in range(len(conv_channels) - 1)
-            ],
-                
+            ])
+                    
             # dropout
-            nn.Dropout(dropout_rate)
-        )
+            self.dropout = nn.Dropout(dropout_rate)
+            
 
-        self.classifier = nn.Sequential(
-            # linear layers
-            nn.AdaptiveAvgPool2d(output_size=(4, 4)),
-            nn.Flatten(),
-            nn.Linear(in_features=4 * 4 * conv_channels[-1], out_features=linear_features[0]),
-            nn.ReLU(),
-            nn.Linear(in_features=linear_features[0], out_features=num_classes)
-        )
-        #######################
-        # END OF YOUR CODE    #
-        #######################
+            self.classifier = nn.Sequential(
+                # linear layers
+                nn.AdaptiveAvgPool2d(output_size=(4, 4)),
+                nn.Flatten(),
+                nn.Linear(in_features=4 * 4 * conv_channels[-1], out_features=linear_features[0]),
+                nn.ReLU(),
+                nn.Linear(in_features=linear_features[0], out_features=num_classes)
+            )
+        else:
+            #######################
+            # PUT YOUR CODE HERE  #
+            #######################
+            self.layers = nn.Sequential(
+                # conv block 1
+                nn.Conv2d(in_channels=in_channels, out_channels=conv_channels[0], kernel_size=3, padding=1),
+                nn.BatchNorm2d(conv_channels[0]),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),
+
+                # conv block 2 to n
+                *[
+                    nn.Sequential(
+                        nn.Conv2d(in_channels=conv_channels[i], out_channels=conv_channels[i+1], kernel_size=3, padding=1),
+                        nn.BatchNorm2d(conv_channels[i+1]),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2, 2)
+                    ) for i in range(len(conv_channels) - 1)
+                ],
+                    
+                # dropout
+                nn.Dropout(dropout_rate)
+            )
+
+            self.classifier = nn.Sequential(
+                # linear layers
+                nn.AdaptiveAvgPool2d(output_size=(4, 4)),
+                nn.Flatten(),
+                nn.Linear(in_features=4 * 4 * conv_channels[-1], out_features=linear_features[0]),
+                nn.ReLU(),
+                nn.Linear(in_features=linear_features[0], out_features=num_classes)
+            )
+            #######################
+            # END OF YOUR CODE    #
+            #######################
 
     def forward(self, x):
-        x = self.layers(x)
-        x = self.classifier(x)
+        if self.use_residuals:
+            x = self.block1(x)
+            for res_block in self.res_blocks:
+                residual = x  # Save the input
+                x = res_block(x)
+                if residual.shape == x.shape:  # Add skip connection only if dimensions match
+                    x += residual
+            x = self.dropout(x)
+            x = self.classifier(x)
+        else:   
+            x = self.layers(x)
+            x = self.classifier(x)
         return x
 
 
